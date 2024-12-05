eec3b84f2a49b860a4ac5bd37b642c6c2a4aacb6
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2024-12-05 01:54:35 -0600

    Implementation of the cloud rendering with transformations

--- main.cpp
+++ main.cpp
@@ -1,12 +1,83 @@
 #include "ui/sdl_window.h"
 #include "core/obj_parser.h"
 #include "core/rasterization/cloud.h"
+#include "core/transformation/transformation.h"
 
 bool isRunning = false;
 
-// Free the resources that must be released before the application is closed.
-void freeResources(){
+void processInput(void);
+Model update(Model, TransformationCoefficients);
+void render(SDLWindow*, Canvas, Model, Perspective, Cloud);
+void freeResources(void);
 
+int main(int argc, char* argv[]) {
+
+    // Initial test configuration
+    RenderConfiguration configuration{
+        .width = 800,
+        .height = 600,
+        .fov = 160,
+        .znear = 100,
+        .zfar = 0.01,
+        .fps = 15
+    };
+
+    // Initial configuration of the transformation
+    TransformationCoefficients transformations{
+        .escalationX = 1.0,
+        .escalationY = 1.0,
+        .escalationZ = 1.0,
+        .translationX = 0.0,
+        .translationY = 0.0,
+        .translationZ = 5,
+        .rotationX = 0.5,
+        .rotationY = 0.5,
+        .rotationZ = 0.5,
+    };
+
+    float frameTargetTime = (1000 / configuration.fps);
+    uint32_t previous_frame_time = 0;
+
+    SDLWindow window(configuration.width, configuration.height);
+    isRunning = window.isRunning();
+
+    ObjParser parser;
+    Cloud cloud;
+
+    Canvas canvas(configuration.width, configuration.height);
+    char *filename = "C:\\Users\\gabri\\CLionProjects\\3d-renderer-engine\\assets\\models\\cube.obj";
+    Model model = parser.readFile(filename);
+    Perspective perspectiveProjection(configuration);
+
+
+    while (isRunning){
+        processInput();
+
+        transformations.rotationX += 0.1;
+        transformations.rotationY += 0.1;
+        transformations.rotationZ += 0.1;
+
+        Model m = update(model,transformations);
+        render(&window,
+               canvas,
+               m,
+               perspectiveProjection,
+               cloud);
+
+        // Guarantees that every frame is executed every FRAME_TARGET_TIME
+        // Wait some time until the reach the target frame time in milliseconds
+        int time_to_wait = frameTargetTime - (SDL_GetTicks() - previous_frame_time);
+
+        // Only delay execution if we are running too fast
+        if(time_to_wait >0 && time_to_wait <= frameTargetTime){
+            SDL_Delay(time_to_wait);
+        }
+        previous_frame_time = SDL_GetTicks();
+    }
+
+    window.destroy();
+    freeResources();
+    return 0;
 }
 
 void processInput(void){
@@ -23,63 +94,27 @@ void processInput(void){
     }
 }
 
-void update(void){
+Model update(Model model, TransformationCoefficients transformationCoefficients){
 
+    Transformation transformer;
+    auto transformedModel = transformer.transformModel(model, transformationCoefficients);
+
+    return transformedModel;
 }
 
-void render(void){
-
-}
-
-int main(int argc, char* argv[]) {
-
-    // Initial test configuration
-    RenderConfiguration configuration{
-        .width = 800,
-        .height = 600,
-        .fov = 160,
-        .znear = 100,
-        .zfar = 0.01,
-        .fps = 60
-    };
-
-    float frameTargetTime = (1000 / configuration.fps);
-    uint32_t previous_frame_time = 0;
-
-    SDLWindow window(configuration.width, configuration.height);
-    isRunning = window.isRunning();
-
-    Model model;
-    ObjParser parser;
-    Cloud cloud;
-
-    Canvas canvas(configuration.width, configuration.height);
-    char *filename = "C:\\Users\\gabri\\CLionProjects\\3d-renderer-engine\\assets\\models\\cube.obj";
-    parser.readFile(filename, model);
+void render(SDLWindow *window,
+            Canvas canvas,
+            Model model,
+            Perspective perspective,
+            Cloud cloud){
 
     canvas.drawGrid();
-    Perspective perspectiveProjection(configuration);
-    cloud.raster(canvas, model, perspectiveProjection);
+    cloud.raster(canvas, model, perspective);
+    window->update(canvas);
+    canvas.clear(0xFF000000);
+}
 
-    window.update(canvas);
+// Free the resources that must be released before the application is closed.
+void freeResources(){
 
-    while (isRunning){
-        processInput();
-        update();
-        render();
-
-        // Guarantees that every frame is executed every FRAME_TARGET_TIME
-        // Wait some time until the reach the target frame time in milliseconds
-        int time_to_wait = frameTargetTime - (SDL_GetTicks() - previous_frame_time);
-
-        // Only delay execution if we are running too fast
-        if(time_to_wait >0 && time_to_wait <= frameTargetTime){
-            SDL_Delay(time_to_wait);
-        }
-        previous_frame_time = SDL_GetTicks();
-    }
-
-    window.destroy();
-    freeResources();
-    return 0;
 }
