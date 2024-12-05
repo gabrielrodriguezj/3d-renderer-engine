32c87c3e74b8efb67fd94f7146401ab1bd08f950
Gabriel Rodríguez <gabrielrodriguezj@hotmail.com>
2024-12-05 03:18:05 -0600

    Correction of bug related to the projection and renderer class

--- core/rendering/cloud.cpp
+++ core/rendering/cloud.cpp
@@ -1,9 +1,9 @@
 #include "cloud.h"
 
-void Cloud::render(Canvas canvas, Model model, Projection projecter) {
+void Cloud::render(Canvas canvas, Model model, Projection *projecter) {
 
     for (vec3_t vertex: model.getVertices()) {
-        vec2_t projectedPoint = projecter.project(vertex);
+        vec2_t projectedPoint = projecter->project(vertex);
 
         canvas.drawRect(projectedPoint.x - 3, projectedPoint.y - 3, 6, 6, 0xFF00FFFF);
     }
